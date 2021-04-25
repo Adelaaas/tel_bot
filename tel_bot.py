0092c0e675b44ff821d209d7c65750b8dd4332e9
Adelaaas <adela.x@yandex.ru>
2021-04-25 21:52:13 +0300

    dominotory

--- tel_bot.py
+++ tel_bot.py
@@ -1,15 +1,42 @@
 import telebot
+# Импортируем типы из модуля, чтобы создавать кнопки
+from telebot import types
+
 bot = telebot.TeleBot('1786855905:AAGhI5rJX-StfEnQfNVQsh3RVSm5Z4EaTYw')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+    bot.reply_to(message, f'Я бот, созданный помогать студентам НИЯУ МИФИ. Приятно познакомиться, {message.from_user.first_name}')
 
+
+# Метод, который получает сообщения и обрабатывает их
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
+    # Если написали «Привет»
+    if message.text == "Привет":
+        # Пишем приветствие
+        bot.send_message(message.from_user.id, "Привет, что ты хочешь узнать?.")
+        # Готовим кнопки
+        keyboard = types.InlineKeyboardMarkup()
+        # По очереди готовим текст и обработчик для каждого знака зодиака
+        key_oven = types.InlineKeyboardButton(text='Как оплатить общежитие онлайн?', callback_data='dominotory')
+        # И добавляем кнопку на экран
+        keyboard.add(key_oven)
+        # Показываем все кнопки сразу и пишем сообщение о выборе
+        bot.send_message(message.from_user.id, text='Выбери интересующий вопрос', reply_markup=keyboard)
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
     else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+# Обработчик нажатий на кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    # Если нажали на одну из 12 кнопок — выводим гороскоп
+    if call.data == "dominotory": 
+        # Формируем гороскоп
+        msg = 'Зайти в раздел "Сервисы" на home.mephi.ru'
+        # Отправляем текст в Телеграм
+        bot.send_message(call.message.chat.id, msg)
 
 bot.polling(none_stop=True)
