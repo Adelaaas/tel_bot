73bd8ce2f41f590d027a0e64756f196b5c2813cc
Adelaaas <adela.x@yandex.ru>
2021-04-26 22:49:01 +0300

    hello

--- tel_bot.py
+++ tel_bot.py
@@ -1,8 +1,11 @@
 import telebot
 # Импортируем типы из модуля, чтобы создавать кнопки
 from telebot import types
+import pandas as pd
 
 bot = telebot.TeleBot('1786855905:AAGhI5rJX-StfEnQfNVQsh3RVSm5Z4EaTYw')
+
+df = pd.read_csv('test5.csv')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -15,13 +18,17 @@ def get_text_messages(message):
     # Если написали «Привет»
     if message.text == "Привет":
         # Пишем приветствие
-        bot.send_message(message.from_user.id, "Привет, что ты хочешь узнать?.")
+        bot.send_message(message.from_user.id, "Привет, что ты хочешь узнать?")
+        bot.send_message(message.from_user.id, df.iloc[0,1])
         # Готовим кнопки
         keyboard = types.InlineKeyboardMarkup()
         # По очереди готовим текст и обработчик для каждого знака зодиака
-        key_oven = types.InlineKeyboardButton(text='Как оплатить общежитие онлайн?', callback_data='dominotory')
+        key_dom_oplata = types.InlineKeyboardButton(text='Как оплатить общежитие онлайн?', callback_data='dominotory')
         # И добавляем кнопку на экран
-        keyboard.add(key_oven)
+        keyboard.add(key_dom_oplata)
+        key_dom_kvit = types.InlineKeyboardButton(text='Как получить квитанцию на оплату общежития онлайн?', callback_data='dom_kvit')
+        # И добавляем кнопку на экран
+        keyboard.add(key_dom_kvit)
         # Показываем все кнопки сразу и пишем сообщение о выборе
         bot.send_message(message.from_user.id, text='Выбери интересующий вопрос', reply_markup=keyboard)
     elif message.text == "/help":
@@ -35,9 +42,19 @@ def callback_worker(call):
     # Если нажали на одну из 12 кнопок — выводим гороскоп
     if call.data == "dominotory": 
         # Формируем гороскоп
-        msg = 'Зайти в раздел "Сервисы" на home.mephi.ru'
+        msg1 = '1. Зайти в интернет-банк или мобильное приложение «ИПБ-Онлайн»'
+        msg2 = '2. В разделе «Платежи и переводы» перейти в подраздел «Банковские операции»'
+        msg3 = '3. Выбрать нужную услугу – «МИФИ (Общежитие, коммунальные и образовательные услуги)» или «МИФИ (Прочие услуги)»'
+        msg4 = '4. На форме оплаты выбрать назначение платежа, ввести внутренний счет (указан в квитанции), номер договора (при наличии) и ФИО (при оплате за третье лицо)'
         # Отправляем текст в Телеграм
-        bot.send_message(call.message.chat.id, msg)
-        bot.send_photo(call.message.chat.id, photo=open('bee-on-daisy.jpg', 'rb'))
-
+        bot.send_message(call.message.chat.id, msg1 + '\n' + msg2+ '\n' + msg3 + '\n' + msg4)
+        bot.send_photo(call.message.chat.id, photo=open('оплата-01.png', 'rb'))
+    if call.data == "dom_kvit": 
+        # Формируем гороскоп
+        msg1 = '1. Зайти в раздел сервисы на home.mephi.ru'
+        msg2 = '2. Выбери "Квитанции на оплату общежития"'
+        msg3 = '3. Выбери необходимы период, за который хочешь оплатить общагу'
+        # Отправляем текст в Телеграм
+        bot.send_message(call.message.chat.id, msg1 + '\n' + msg2+ '\n' + msg3)
+        bot.send_photo(call.message.chat.id, photo=open('квитанция онлайн.png', 'rb'))
 bot.polling(none_stop=True)
